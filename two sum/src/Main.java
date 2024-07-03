fbc2f74ebb28e930eeda58a5997eb1c2a5a6ca22
Fai-chebby <chebetf034@gmail.com>
2024-07-03 13:33:58 +0300

    Two sum solution

--- two sum/src/Main.java
+++ two sum/src/Main.java
@@ -1,5 +1,8 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+class Solution{
+    public int[] twoSum(int[]nums,int target){
+        for (int i=0;i<nums.length; i++){
+            for ()
+        }
+
     }
 }
