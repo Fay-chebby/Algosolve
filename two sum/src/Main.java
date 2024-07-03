37b8ec683cc037a55f18800f217fcf4ba308486e
Fai-chebby <chebetf034@gmail.com>
2024-07-03 13:35:30 +0300

    For loop in the solution

--- two sum/src/Main.java
+++ two sum/src/Main.java
@@ -1,7 +1,8 @@
 class Solution{
     public int[] twoSum(int[]nums,int target){
         for (int i=0;i<nums.length; i++){
-            for ()
+            for (int j=i+1;j< nums.length;j++)
+                if ()
         }
 
     }
